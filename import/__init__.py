75efaef53539ee08c8194d21e0f754201fd7e40f
ds12a <dshen1248@gmail.com>
2026-01-24 15:05:28 -0500

    more docs

--- import/__init__.py
+++ import/__init__.py
@@ -1,3 +1,14 @@
 """
-.. include:: ../README.md
+Module for track generation.
+
+usage: import.py [-h] --gpx_source GPX_SOURCE [--track_destination TRACK_DESTINATION] [--config CONFIG] [--plot]
+
+options:
+  -h, --help            show this help message and exit
+  --gpx_source GPX_SOURCE
+                        Source path to track gpx file.
+  --track_destination TRACK_DESTINATION
+                        Destination path of fitted track.
+  --config CONFIG       Path to config file.
+  --plot                Toggles on plotting.
 """
