bdd1259fc5ba0cdb16f51f9fc2e68ee8e4f96dcd
ds12a <dshen1248@gmail.com>
2026-01-24 20:16:29 -0500

    docs

--- import/__init__.py
+++ import/__init__.py
@@ -1,14 +1,19 @@
 """
-Module for track generation.
+## Module for track generation.
 
-usage: ```python import.py [-h] --gpx_source GPX_SOURCE [--track_destination TRACK_DESTINATION] [--config CONFIG] [--plot]```
+**usage**: ```python import.py [-h] --gpx_source GPX_SOURCE [--track_destination TRACK_DESTINATION] [--config CONFIG] [--plot]```
 
-options:
+**options**:
+
   ```-h```, ```--help```            show this help message and exit
+
   ```--gpx_source GPX_SOURCE```
                         Source path to track gpx file.
+
   ```--track_destination TRACK_DESTINATION```
                         Destination path of fitted track.
+
   ````--config CONFIG```       Path to config file.
+
   ```--plot```                Toggles on plotting.
 """
